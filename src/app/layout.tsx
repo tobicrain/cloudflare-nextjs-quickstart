41370f1588ae366efd097300d86412ab74516e91
Tobias Krainhöfner <tobias@krainhoefner.com>
2024-12-28 16:08:02 +0100

    layout.tsx aktualisieren

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,13 +16,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
       suppressHydrationWarning
     >
       <head>
-<meta charset="utf-8" />
+    <meta charset="utf-8" />
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" /> -->
     <title>Random</title>
     <link rel="manifest" href="/manifest.json" />
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="/style.css"/>
     
     <!-- The `react-native-web` recommended style reset: https://necolas.github.io/react-native-web/docs/setup/#root-element -->
     <style id="expo-reset">
@@ -41,9 +41,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
         height: 100%;
         flex: 1;
       }
-    </style>
-</head>
-      <link rel="manifest" href="/manifest.json" />
+      </style>
+    </head>
       <body className={clsx('bg-gray-950 text-gray-50 p-4', fontFamily.variable)}>{children}</body>
     </html>
   );
